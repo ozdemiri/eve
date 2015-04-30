1ca6df6c7c76612524f6de84659543a6ae09a1ed
Ismail Ozdemir <ismailozdemir3@gmail.com>
2015-04-30 15:28:16 +0300

    Demo pages added

---
+++ demo/js/sampledata.js
@@ -0,0 +1,15 @@
+﻿//declare pie chart data
+var pieData = [
+    { ageGroup: '6-21 yo', population: 14397 },
+    { ageGroup: '22-44 yo', population: 64508 },
+    { ageGroup: '45-60 yo', population: 68765 },
+    { ageGroup: '60+ yo', population: 274472 }
+];
+
+//declare donut chart data
+var donutData = [
+    { ageGroup: '6-21 yo', population: 39382 },
+    { ageGroup: '22-44 yo', population: 276517 },
+    { ageGroup: '45-60 yo', population: 465700 },
+    { ageGroup: '60+ yo', population: 1339550 }
+];
